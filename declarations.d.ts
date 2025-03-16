7f1b9b3e4589c6e36d252800d58ce9cc362e2169
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-03-16 17:36:38 +0800

    Add TypeScript declarations for image and SVG modules

---
+++ declarations.d.ts
@@ -0,0 +1,16 @@
+// For SVG files
+declare module '*.svg' {
+    import React from 'react';
+    import { SvgProps } from 'react-native-svg';
+    const content: React.FC<SvgProps>;
+    export default content;
+  }
+  
+  
+  // For images (optional, helps with TypeScript)
+  declare module '*.png';
+  declare module '*.jpg';
+  declare module '*.jpeg';
+  declare module '*.gif';
+  declare module '*.webp';
+  
